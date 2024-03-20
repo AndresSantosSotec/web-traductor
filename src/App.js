faa696d42888fcaed8ef710a40e5841f4234ad93
AndresDev <mlpdbz300@gmail.com>
2024-03-20 13:01:54 -0600

    Este es el primer Comit de la app en react juanito

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
+
+
+import Contact from './Pantallas/Contact';
+import Benefits from './Pantallas/Benefits';
+import HowWeWork from './Pantallas/HowWeWork';
+import Services from './Pantallas/Services';
+//import  Principal  from './Pantallas/Principal';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Llamar a cada componente */}
+      
+      <Contact />
+      <Benefits />
+      <HowWeWork />
+      <Services />
     </div>
   );
 }
---
+++ src/Pantallas/Contact.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Contact.module.css'; // Archivo de estilos personalizado
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card mt-5">
+            <div className="card-header bg-primary text-white">Contact</div>
+            <div className="card-body">
+              {/* Contenido del formulario de contacto */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
